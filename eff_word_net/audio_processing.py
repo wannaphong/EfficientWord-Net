22e0fc2fdbaad61956a649e499dfdedebaa605d0
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2024-01-24 12:46:52 +0700

    Update audio_processing.py

--- eff_word_net/audio_processing.py
+++ eff_word_net/audio_processing.py
@@ -1,5 +1,4 @@
 import glob
-import tflite_runtime.interpreter as tflite
 import os
 import numpy as np
 import random
@@ -62,86 +61,7 @@ class ModelRawBackend :
     def audioToVector(self, inpAudio:np.array) -> np.array :
         raise NotImplementedError("Vector Convertion on raw model backend invoked")
 
-class First_Iteration_Siamese(ModelRawBackend) :
-    def __init__(self) :
-        self.window_length = 1.0 # 1 second
-        self.window_frames = int(self.window_length * 16000)     
-        self.logmelcalc_interpreter = tflite.Interpreter(
-                model_path=os.path.join(LIB_FOLDER_LOCATION,"models/first_iteration_siamese/logmelcalc.tflite"
-            )
-        )
-        self.logmelcalc_interpreter.allocate_tensors()
-        
-        self.input_index = self.logmelcalc_interpreter.get_input_details()[0]["index"]
-        self.output_details = self.logmelcalc_interpreter.get_output_details()
-        
-        self.baseModel_interpreter = tflite.Interpreter(
-              model_path=os.path.join(LIB_FOLDER_LOCATION,"models/first_iteration_siamese/baseModel.tflite")
-          )
-        self.baseModel_interpreter.allocate_tensors()
-        
-        self.base_model_inp = self.baseModel_interpreter.get_input_details()
-        self.base_model_out = self.baseModel_interpreter.get_output_details()
 
-
-    def scoreVector(self, inp_vec, embeddings):
-        """
-        **Use this directly only if u know what you are doing**
-
-        Returns a float with confidence of match 0 - 1
-        """
-
-        assert inp_vec.shape == (1,128), \
-            "Inp vector should be of shape (1,128)"
-
-        distances = np.sqrt(
-            np.sum(
-                (inp_vec - embeddings)**2,
-                axis=1
-            )
-        )
-
-        distances[distances>0.3] = 0.3
-        top3 = (0.3-np.sort(distances)[:3])/0.3
-        out = 0.0
-        for i in top3 :
-            out+= (1-out) * i
-
-        return out
-
-
-    def audioToVector(self, inpAudio:np.array) -> np.array :
-        """
-        Converts 16000Hz sampled 1 sec of audio to vector embedding
-        Inp Parameters :
-    
-            inpAudio  : np.array of shape (16000,)
-    
-        Out Parameters :
-    
-            1 vector embedding of shape (128,1)
-    
-        """
-        assert(inpAudio.shape==(self.window_frames,))
-    
-        self.logmelcalc_interpreter.set_tensor(
-            self.input_index,
-            np.expand_dims(
-                inpAudio/inpAudio.max(),
-                axis=0
-            ).astype("float32")
-        )
-        self.logmelcalc_interpreter.invoke()
-        self.logmel_output = self.logmelcalc_interpreter.get_tensor(self.output_details[0]['index'])
-        self.baseModel_interpreter.set_tensor(
-            self.base_model_inp[0]["index"],
-            np.expand_dims(self.logmel_output,axis=(0,-1)).astype("float32")
-        )
-        self.baseModel_interpreter.invoke()
-        output_data = self.baseModel_interpreter.get_tensor(self.base_model_out[0]['index'])
-    
-        return output_data
-    
 class Resnet50_Arc_loss(ModelRawBackend):
     def __init__(self):
         
@@ -219,10 +139,8 @@ class Resnet50_Arc_loss(ModelRawBackend):
 from enum import Enum
 
 class ModelType(str, Enum):
-    first_iteration_siamese = "first_iteration_siamese"
     resnet_50_arc = "resnet_50_arc"
 
 MODEL_TYPE_MAPPER = {
-    "first_iteration_siamese" : First_Iteration_Siamese,
     "resnet_50_arc" : Resnet50_Arc_loss
 }
